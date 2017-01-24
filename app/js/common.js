7674ba4b7257aac939705150021d3a7800de632f
skatt931 <i.kurnytskyi@gmail.com>
2017-01-24 20:42:14 +0200

    drams plaing

--- app/js/common.js
+++ app/js/common.js
@@ -1,28 +1,24 @@
-$(function() {
+$(function () {
 
-	//SVG Fallback
-	if(!Modernizr.svg) {
-		$("img[src*='svg']").attr("src", function() {
-			return $(this).attr("src").replace(".svg", ".png");
-		});
-	};
+    function playSounds(e) {
+        const audio = document.querySelector(`audio[data - key = "${e.keyCode}"]`)
+        ;
+        const key = document.querySelector(`.
+        key[data - key = "${e.keyCode}"]`)
+        ;
+        if (!audio) return //stop audio from playing
+        audio.currentTime = 0; //fewind from the 0
+        audio.play();
+        key.classList.add('plaing');
+    }
 
-	//E-mail Ajax Send
-	//Documentation & Example: https://github.com/agragregra/uniMail
-	$("form").submit(function() { //Change
-		var th = $(this);
-		$.ajax({
-			type: "POST",
-			url: "mail.php", //Change
-			data: th.serialize()
-		}).done(function() {
-			alert("Thank you!");
-			setTimeout(function() {
-				// Done Functions
-				th.trigger("reset");
-			}, 1000);
-		});
-		return false;
-	});
+    function removeTransition(e) {
+        if (e.propertyName !== 'transform') return; //skip it if it is not transform
+        this.classList.remove("plaing");
+    }
+
+    const keys = document.querySelectorAll('.key');
+    keys.forEach(key = > key.addEventListener('transitionend', removeTransition));
+    window.addEventListener('keydown', playSounds);
 
 });
